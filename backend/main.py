c870630c1486a1cd888d75d3a1f7fdb3431599cc
Ilya Maksimov <dilershina@gmail.com>
2025-10-07 23:01:02 +0300

    added first version of model

--- backend/main.py
+++ backend/main.py
@@ -6,9 +6,12 @@ from .config import settings
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import BotCommand
+from aiogram.types import BotCommand, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
+from .chat import register_handlers as chat
+from .chat import Form
+from aiogram.fsm.context import FSMContext
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -18,11 +21,17 @@ storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
 
-@dp.message(Command("start"))
-async def cmd_start(message: Message):
-    
-    await message.answer("Добро пожаловать")
 
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: Message, state: FSMContext):
+    await state.set_state(Form.waiting_for_message)
+    await message.answer("Отправьте ваше резюме")
+
+
+def register_handlers(dp: Dispatcher):
+    chat(dp)
 
 
 async def main():
@@ -31,6 +40,8 @@ async def main():
     await bot.set_my_commands([
         BotCommand(command="start", description="Запуск бота")
     ])
+
+    register_handlers(dp)
 
     logger.info("Бот запущен")
     await dp.start_polling(bot)
